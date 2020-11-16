6ef8337ea2e22cdc6e0c5b70ea2a96bd8cdca1c0
DenisCo1100100 <kodek_den2001@mail.ru>
2020-11-16 11:21:21 +0300

    Added function authorization

--- UserAccount/Authorization/Authentication.cs
+++ UserAccount/Authorization/Authentication.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace AIS_Kinoteatr
 {
     class Authentication
     {
+        private string Login { get; set; }
+        private string Password { get; set; }
+        private string Position { get; set; }
+
+        private OleDbConnection dbConnection;
+
+        public Authentication(string login, string password)
+        {
+            dbConnection = new OleDbConnection($@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={Application.StartupPath}\RegisteredUsers.mdb;");
+            dbConnection.Open();
+
+            string query = $"SELECT * FROM Users WHERE Login = '{login}' AND Password = '{password}'";
+            OleDbCommand command = new OleDbCommand(query, dbConnection);
+
+            OleDbDataReader reader = command.ExecuteReader();
+
+            while (reader.Read()) 
+            {
+                Login = reader.GetString(1);
+                Password = reader.GetString(2);
+                Position = reader.GetString(3);
+            } 
+
+            dbConnection.Close();
+            reader.Close();
+        }
+
+        public void RunAuthentication()
+        {
+            if (Login != null)
+            {
+                Cinema cinema = new Cinema();
+                cinema.Show();
+            }
+            else
+            {
+                MessageBox.Show($"Аккаунт не зарегистрирован!", "Внимание!");
+            }
+        }
     }
 }
